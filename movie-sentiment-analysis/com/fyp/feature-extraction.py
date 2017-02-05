1f31ffe7902c0fd71486aa308b834e12a7381508
anoukh <anoukh.ashley@gmail.com>
2017-02-05 14:06:51 +0530

    Classifier Complete

--- movie-sentiment-analysis/com/fyp/feature-extraction.py
+++ movie-sentiment-analysis/com/fyp/feature-extraction.py
@@ -1,6 +1,8 @@
 import nltk
 import csv
 
+word_features = []
+
 
 def main():
 
@@ -17,9 +19,16 @@ def main():
         words_filtered = [e.lower() for e in words.split() if len(e)>=3]
         reviews.append((words_filtered, sentiment))
 
+    global word_features
     word_features = get_word_features(get_words_in_reviews(reviews))
-    print len(word_features)
-    print word_features
+    training_set = nltk.classify.apply_features(extract_features, reviews, labeled=True)
+    print len(training_set)
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+    print classifier.show_most_informative_features(60)
+
+    tweet = 'I hate this pathetic movie'
+    print classifier.classify(extract_features(tweet.split()))
 
 
 def get_words_in_reviews(reviews):
@@ -31,8 +40,18 @@ def get_words_in_reviews(reviews):
 
 def get_word_features(word_list):
     word_list = nltk.FreqDist(word_list)
+    global word_features
     word_features = word_list.keys()
     return word_features
+
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
 
 if __name__ == '__main__':
     main()
